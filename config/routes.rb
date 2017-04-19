2de9ebeb97e94b14da905b9a327adbf97eb250fb
codingInSpace <jonathan.grangien@gmail.com>
2017-04-19 14:30:17 +0200

    Mount routes on /api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  resources :projects do
-  	resources :tags
+  scope '/api' do
+		resources :projects do
+			resources :tags
+		end
 	end
 end
