f1b3592c9b7088a4caa78c7fb9593e94de7de1bd
codingInSpace <jonathan.grangien@gmail.com>
2017-04-19 15:03:33 +0200

    Implement webpack dev config

---
+++ frontend/webpack.config.dev.js
@@ -0,0 +1,91 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ROOT_PATH = path.resolve(__dirname);
+
+const styleLoaders = [{
+    loader: 'style-loader',
+  }, {
+    loader: 'css-loader',
+  	options: {
+      localIdentName: '[path]___[name]__[local]___[hash:base64:5]',
+      modules: true
+    }
+  }, {
+    loader: 'postcss-loader',
+  }, {
+    loader: 'sass-loader',
+    options: {
+      sourceMap: true
+    }
+}];
+
+module.exports = {
+	devtool: 'source-map',
+	entry: [
+    'react-hot-loader/patch',
+		'webpack-dev-server/client?http://localhost:1337',
+		'webpack/hot/dev-server',
+    'babel-polyfill',
+		path.resolve(ROOT_PATH, 'src/index')
+	],
+	resolve: {
+		extensions: ['.js', '.jsx', '.json', '.scss', '.css']
+	},
+	output: {
+		path: path.resolve(ROOT_PATH, 'public'),
+		filename: 'bundle.js',
+		publicPath: '/'
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env.API_HOST': JSON.stringify(process.env.API_HOST),
+			'process.env.NODE_ENV': JSON.stringify('development')
+		}),
+    new ExtractTextPlugin({
+      filename: 'styles.css',
+      allChunks: true
+    }),
+		new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loaders: 'babel-loader',
+			},
+			{
+				test: /\.module\.scss$/,
+        use: styleLoaders
+			},
+			{
+				test: /\.scss$/,
+				exclude: [/\.module\.scss$/],
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+                includePaths: [
+                  __dirname + '/node_modules'
+                ],
+                outputStyle: 'compressed'
+              }
+            }
+          ]
+			},
+      {
+        test: /\.(jpg|png)$/,
+        loader: 'file?name=[path][name].[hash].[ext]'
+      }
+		]
+	}
+};
