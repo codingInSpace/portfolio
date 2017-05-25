9fd131273688969edbee9a37e8d9689722269d71
codingInSpace <jonathan.grangien@gmail.com>
2017-05-25 19:40:06 +0200

    Implement fixed token renewal

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,13 @@ class SessionsController < Devise::SessionsController
 	include ActionController::MimeResponds
 
 	acts_as_token_authentication_handler_for User, fallback: :none
+	before_action :require_auth!, only: [:destroy]
+	skip_before_action :verify_signed_out_user, only: :destroy
 
 	def create
 		# Find user for given email
 		user = User.where(email: params[:email]).first
-		#puts #{(User.where(email: params[:email]).first)}
-		
+
 		# If no user found or password invalid, send unauthorized 
 		if user&.valid_password?(params[:password])
 			render json: user.as_json(only: [:id, :email, :authentication_token]), status: :created
@@ -27,17 +28,16 @@ class SessionsController < Devise::SessionsController
 	end
 
 	def destroy
-		sign_out(user)
 		current_user.authentication_token = nil
 		current_user.save
+		sign_out(current_user)
 		head :no_content
 	end
-	
+
 	private
 
-	def after_successful_token_authentication
-		# Make the authentication token disposable
-		renew_authentication_token!
+	def require_auth!
+		throw(:warden, scope: :user) unless current_user.presence
 	end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'sessions' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  devise_scope :user do
+    post 'login' => 'sessions#create', :as => :login
+    delete 'logout' => 'sessions#destroy', :as => :logout
+  end
+
   scope '/api' do
 		resources :projects do
 			resources :tags
