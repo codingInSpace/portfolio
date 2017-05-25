00653ce0d71300b197a317c3b9e0cf99c53208a0
codingInSpace <jonathan.grangien@gmail.com>
2017-05-25 11:19:51 +0200

    Make authentication tokens renew

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::API
-	acts_as_token_authentication_handler_for User, fallback: :exception
-
 	include Response
   include ExceptionHandler
+
+	acts_as_token_authentication_handler_for User, fallback: :exception
+
+	private
+
+	def after_successful_token_authentication
+		# Make the authentication token disposable
+		renew_authentication_token!
+	end
 end
+
