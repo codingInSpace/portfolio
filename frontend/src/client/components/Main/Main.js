8582d52b1b62d56b26aaac8f13250977c8926d7d
codingInSpace <jonathan.grangien@gmail.com>
2017-09-25 20:26:23 +0200

    Redesign app banner

--- frontend/src/client/components/Main/Main.js
+++ frontend/src/client/components/Main/Main.js
@@ -1,44 +1,15 @@
 import React from 'react'
 
-import Box from 'grommet/components/Box'
-import Paragraph from 'grommet/components/Paragraph'
-import Heading from 'grommet/components/Heading'
-import Label from 'grommet/components/Label'
-import Button from 'grommet/components/Button'
-import Anchor from 'grommet/components/Anchor'
-import LinkNext from 'grommet/components/icons/base/LinkNext'
-import MailIcon from 'grommet/components/icons/base/Mail'
-import LinkedInIcon from 'grommet/components/icons/base/SocialLinkedinOption'
-import GithubIcon from 'grommet/components/icons/base/SocialGithub'
 import cssModules from 'react-css-modules'
-
 import setHeaderView from '../../../shared/HOC/setHeaderView'
 import provideWindowWidth from '../../../shared/HOC/provideWindowWidth'
 
 import AppBanner from '../../containers/AppBanner'
 import Portfolio from '../../containers/Portfolio'
-import descMain from './text'
 import * as styles from './index.module.scss'
-
-const SocialLink = (props) => {
-  return (props.link === undefined) ? (
-      <Box align="center" justify="start" direction="row">
-        {props.icon}
-        <Box pad="small"><Label truncate margin="none">{props.label}</Label></Box>
-      </Box>
-  ) : (
-    <Anchor href={props.link}
-            target="_blank"
-            align="center"
-            label={<Label margin="none">{props.label}</Label>}
-            icon={props.icon} />
-  )
-}
 
 let Main = (props) => {
   const isOffset = props.bannerOffset > 0
-  const mobileBreak = props.width < 724
-  const largeHorPad = { horizontal: 'large', vertical: 'none' }
 
   const offsetStyle = {
     paddingTop: props.bannerOffset
@@ -49,21 +20,6 @@ let Main = (props) => {
 			<AppBanner large />
 			<div style={isOffset ? offsetStyle : {}}>
         <Portfolio />
-        <Box pad="large"
-             direction="row"
-             align="center"
-             justify="center">
-          <Box align="start" pad={largeHorPad}>
-            <SocialLink icon={mobileBreak ? undefined : <MailIcon />}
-                        label="jonathan.grangien@gmail.com" />
-            <SocialLink icon={<GithubIcon />}
-                        link="https://github.com/codingInSpace/"
-                        label="GitHub" />
-            <SocialLink icon={ <LinkedInIcon /> }
-                        link="https://www.linkedin.com/in/jonathan-grangien-630859104/"
-                        label="LinkedIn" />
-          </Box>
-        </Box>
 			</div>
 		</div>
   )
--- frontend/src/client/containers/AppBanner/AppBanner.js
+++ frontend/src/client/containers/AppBanner/AppBanner.js
@@ -2,6 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
+import Label from 'grommet/components/Label'
+import Anchor from 'grommet/components/Anchor'
+import MailIcon from 'grommet/components/icons/base/Mail'
+import LinkedInIcon from 'grommet/components/icons/base/SocialLinkedinOption'
+import GithubIcon from 'grommet/components/icons/base/SocialGithub'
 import Box from 'grommet/components/Box'
 import Image from 'grommet/components/Image'
 import Paragraph from 'grommet/components/Paragraph'
@@ -10,6 +15,7 @@ import Headline from 'grommet/components/Headline'
 import rose from 'violet-sine-rose'
 import cssModules from 'react-css-modules'
 import styles from './index.module.scss'
+import provideWindowWidth from '../../../shared/HOC/provideWindowWidth'
 
 import { SET_HEIGHT_OFFSET_OF_BANNER } from '../../../app'
 
@@ -45,6 +51,9 @@ class AppBanner extends React.Component {
   render() {
     const { primary, secondary, large } = this.props
 
+    const mobileBreak = this.props.width < 724
+    const largeHorPad = { horizontal: 'large', vertical: 'none' }
+
     const TitleDetails = () => (
       <Box pad="medium" align="center" textAlign="left">
         <Headline size="small" className={styles.text}>{primary}</Headline>
@@ -54,13 +63,28 @@ class AppBanner extends React.Component {
       </Box>
     )
 
+    const SocialLink = (props) => {
+      return (props.link === undefined) ? (
+        <Box align="center" justify="start" direction="row">
+          {props.icon}
+          <Box pad="small"><Label truncate margin="none">{props.label}</Label></Box>
+        </Box>
+      ) : (
+        <Anchor href={props.link}
+                target="_blank"
+                align="center"
+                label={<Label margin="none">{props.label}</Label>}
+                icon={props.icon} />
+      )
+    }
+
     return (
       <div className={styles.container}>
         { large ? <div style={{position: 'absolute', zIndex: 0, }} ref={el => this.canvasParentRef = el}></div> : null }
         <Box pad={large ? 'large' : 'small'}
              className={ large ? styles.bannerLarge : styles.bannerSmall }
              align="center">
-          <Box direction="row"
+          <Box direction={large ? 'column' : 'row'}
                align="center"
                pad="large">
             { large ? (
@@ -74,6 +98,16 @@ class AppBanner extends React.Component {
               ) : (
                 <TitleDetails />
               ) }
+          <Box align="start" pad={large ? 'none' : largeHorPad}>
+            <SocialLink icon={mobileBreak ? undefined : <MailIcon />}
+                        label="jonathan.grangien@gmail.com" />
+            <SocialLink icon={<GithubIcon />}
+                        link="https://github.com/codingInSpace/"
+                        label="GitHub" />
+            <SocialLink icon={<LinkedInIcon />}
+                        link="https://www.linkedin.com/in/jonathan-grangien-630859104/"
+                        label="LinkedIn" />
+          </Box>
           </Box>
         </Box>
       </div>
@@ -89,7 +123,7 @@ AppBanner.propTypes = {
 
 AppBanner.defaultProps = {
   primary: 'Jonathan Grangien',
-  secondary: 'Developer based in Sweden with a passion for front end development, computer graphics, and innovative tech.',
+  secondary: 'Developer based in Sweden with a passion for front end development, computer graphics, visualization and innovative tech.',
   large: false,
 }
 
@@ -98,4 +132,5 @@ const mapDispatch = dispatch => ({
 })
 
 AppBanner = connect(null, mapDispatch)(AppBanner)
+AppBanner = provideWindowWidth(AppBanner)
 export default cssModules(AppBanner, styles)
